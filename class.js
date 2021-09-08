de78889848f739e1dd664137776cc6b904aed7be
Prudhvi <prudhvidillu@gmail.com>
2021-09-08 18:32:15 +0530

    new pull request example

--- class.js
+++ class.js
@@ -8,6 +8,7 @@
     var emp1 =new Employee(101,'dilip')
     var emp2 =new Employee(102,'prudhvi')
     
+    console.log("git learn");
     console.log(emp1.id)
     console.log(emp1.name)
     console.log(emp2.id)
