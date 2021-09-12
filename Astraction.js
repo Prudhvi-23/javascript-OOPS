d8874f130ba180ac3cdb95c30b36b2ff46846820
Prudhvi-23 <90323947+Prudhvi-23@users.noreply.github.com>
2021-09-12 18:57:50 +0530

    Add files via upload

--- Astraction.js
+++ Astraction.js
@@ -1,24 +1,23 @@
-function Employee(name,age,basesalary){
-    this.name=name;
-    this.age=age;
-    this.basesalary=basesalary;
-    let monthlybouns=1000
+function Car(color,cost,milage){
+    this.color=color;
+    this.cost=cost;
+    this.milage=milage;
+    let priceIcreaseNextYear=50000
 
-    let CalculateFinalSalary=function(){
-      let finalSalary=basesalary+monthlybouns;
-    console.log('finalSalary is:'+ finalSalary);
+   this.getCarDetails=function(){
+    console.log("color of the car is :"+this.color);
+    console.log("cost of the car is:"+this.cost);
+    console.log("milage of the car is:"+this.milage);
  }
+ this.getNextYearCarPrice=function(){
+  console.log(this.cost+priceIcreaseNextYear);
+}
 
-   this.getEmpDetails=function(){
-    console.log("Name:"+this.name);
-    console.log("Age:"+this.age);
-    console.log("Basesalary:"+this.basesalary);
-    CalculateFinalSalary();
-   
- }
    
 }
-let emp1=new Employee('Prudhvi',23,20000)
-emp1.getEmpDetails();
+let SwiftVDi=new Car('Red',800000,22.17);
+SwiftVDi.getCarDetails();
+SwiftVDi.priceIcreaseNextYear;
+SwiftVDi.getNextYearCarPrice();
 
 
